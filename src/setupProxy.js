bdc1fce77eae1b73a706b8ee651303dbbd1fef2c
raturaihan <63285976+raturaihan@users.noreply.github.com>
2022-02-08 20:39:20 +0700

    Edit end point in setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware")
 
 module.exports = app => {
     app.use(
-        createProxyMiddleware('/Graphic',
+        createProxyMiddleware('/release/loginbiogas',
             {
                 target:'https://ehy3b0lyhk.execute-api.us-east-1.amazonaws.com',
                 changeOrigin: true
